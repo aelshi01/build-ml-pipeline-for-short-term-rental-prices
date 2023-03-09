22c4538f21057214e623b816f732f4d0daa835d9
Adam Elshimi <adamelshimi@adams-air.home>
2023-03-09 08:45:55 +0000

    downloaded dataset, done data cleaning/check and split the data

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    ######################
+    # YOUR CODE HERE     #
+    ######################
+    logger.info("Downloading and preparing artifact")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    logger.info("loading csv file with pandas")
+    df = pd.read_csv(artifact_local_path)
+
+    min_price = args.min_price
+    max_price = args.max_price
+    logger.info("copying new dataframe with new range of values")
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    df.to_csv("clean_sample.csv", index=False)
+    
+    logger.info("logging new artifact of transformed dataset")
+    artifact = wandb.Artifact(
+     args.output_artifact,
+     type=args.output_type,
+     description=args.output_description,
+ )
+    logger.info("uploading and adding new transformed dataset to W&B")
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,## INSERT TYPE HERE: str, float or int,
+        help="the artifact you want to download from weights and biases",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,## INSERT TYPE HERE: str, float or int,
+        help="name of new transformed artifact",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,## INSERT TYPE HERE: str, float or int,
+        help="The type of job we are doing.  This will be categorised within W&B interface",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,## INSERT TYPE HERE: str, float or int,
+        help="A description about the what we are doing with the artifact and what we are transforming it to",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,## INSERT TYPE HERE: str, float or int,
+        help="the minimum price that should be considered in the dataset",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,## INSERT TYPE HERE: str, float or int,
+        help="The maximum number that should be considered in the dataset",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
